837f5e233f1f37fcb60faed4b85969f0db1d6cab
Edudzi Makafui Akpabey <77601649+EdudziMakafui@users.noreply.github.com>
2022-10-28 20:15:35 +0000

    init

---
+++ includes/ourmessage.php
@@ -0,0 +1,14 @@
+  <section class="py-11 bg-light-gradient border-bottom border-white border-5">
+        <div class="bg-holder overlay overlay-light" style="background-image:url(assets/img/gallery/header-bg.png);background-size:cover;"></div>
+        <!--/.bg-holder-->
+        <div class="container">
+          <div class="row flex-center">
+            <div class="col-12 mb-10">
+              <div class="d-flex align-items-center flex-column">
+                <h3 class="fw-normal"> With an outstanding style, only for you</h3>
+                <h3 class="fs-4 fs-lg-6 fs-md-6 fw-bold">Exclusively designed for you</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
---
+++ index.php
@@ -0,0 +1,61 @@
+<?php include("includes/header.php");?>
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main" id="top">
+    <?php include("includes/navigation.php");?>
+    <?php include("includes/ourmessage.php");?>
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <?php include("includes/for_her_him.php");?>
+      <!-- ============================================-->
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <?php include("includes/best_deal.php");?>
+  <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <?php include("includes/exclusive_collection.php");?>
+
+     <?php include("includes/checkout_new_arrival.php");?>
+     <?php include("includes/categoryMenWomen.php");?>
+    
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <?php include("includes/urbanStories.php");?>
+     <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+     <?php include("includes/best_sellers.php");?>
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <?php include("includes/summer_of_21.php");?>
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <?php include("includes/gentle_formal_looks.php");?>
+    <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <?php include("includes/fashion.php");?>
+     <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+    <?php include("includes/our-branches.php");?>
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <?php include("includes/footer_company_info.php");?>
